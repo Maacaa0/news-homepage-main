4c7cbac112d14b2e1c398bc632728c03837b0230
Maacaa0 <102512313+Maacaa0@users.noreply.github.com>
2022-12-05 19:19:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
 const openMenuBtn = document.querySelector(".menu_btn");
 const closeMenuBtn = document.querySelector(".menu_close_btn");
 const overlay = document.querySelector(".overlay");
-const menu = document.querySelector(".menu");
+const menu = document.querySelector(".mobile_menu");
 const body = document.querySelector("body")
 
 openMenuBtn.addEventListener("click", function() {
         overlay.style.zIndex = "2";
         overlay.style.opacity = ".5";
         menu.style.left = "40vw";
-        menu.style.visibility = "visible";
 })
 
 closeMenuBtn.addEventListener("click", function() {
         overlay.style.zIndex = "-10";
         overlay.style.opacity = "0";
         menu.style.left = "100%";
-        menu.style.visibility = "hidden";
 })
 
 overlay.addEventListener("click", function() {
         overlay.style.zIndex = "-10";
         overlay.style.opacity = "0";
         menu.style.left = "100%";
-        menu.style.visibility = "hidden";
 })
